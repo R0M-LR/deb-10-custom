23b02e7e7faf95cd8e9df8d08aa0ec679429a319
R0M <36591161+R0M-LR@users.noreply.github.com>
2020-04-12 23:30:49 +0200

    Delete stable_php_repo.sh

--- stable_php_repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-apt-get update && apt install gnupg2 -yy
-wget -qO - https://packages.sury.org/php/apt.gpg | apt-key add -
-echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php7.x.list
-apt update
-apt upgrade
